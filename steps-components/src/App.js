b5f0b1b6fa1771ee89200909239c9996002228fa
rajvindergithub <rajvindergithub@gmail.com>
2024-02-18 11:47:33 +0530

    updae step component

--- steps-components/src/App.js
+++ steps-components/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+const colorButtonBlue = {backgroundColor: '#7950f2', color: '#FFF'}; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const messages = ["Learn React", "Apply for Jobs", "Invest your new income"]; 
 
-export default App;
+
+
+ function App(){
+     
+     const step = 1; 
+     
+     function handlePrevious(){
+         alert("Previous");
+     }
+     
+      function handleNext(){
+         alert("Next");
+         
+     }
+     
+     return (
+        <>
+            <div className="steps">
+                <div className="numbers">
+                    <div className={`${step >= 1 ? "active":""}`}>1</div>
+                    <div className={`${step >= 2 ? "active":""}`}>2</div>
+                    <div className={`${step >= 3 ? "active":""}`}>3</div>
+                </div>
+         
+                <p className="message">{step} : {messages[step- 1]}</p>
+                    
+                <div className="buttons">
+                    <button style={colorButtonBlue} onClick={handlePrevious}>Previous</button>
+                    <button style={colorButtonBlue} onClick={handleNext}>Next</button>
+                </div>
+            </div>
+         </>
+     );
+ }
+
+export default App; 
